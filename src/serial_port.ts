b8f3222d010e0908a69de4328c572e8093d48acb
Jeff Hykin <jeff.hykin@gmail.com>
2025-07-18 17:45:18 -0500

    dynamic import to avoid window's 80,000 lines of JS

--- src/serial_port.ts
+++ src/serial_port.ts
@@ -1,32 +1,29 @@
 import type { SerialPortInfo, SerialPort, SerialOptions } from "./common/serial_port.ts"
-import { SerialPortWin } from "./windows/serial_port.ts"
-import { getPortsWin } from "./windows/enumerate.ts"
-import { getPortsDarwin } from "./darwin/enumerate.ts"
-import { SerialPortDarwin } from "./darwin/serial_port.ts"
-import { getPortsLinux } from "./linux/enumerate.ts"
-import { SerialPortLinux } from "./linux/serial_port.ts"
 
-export function getPorts(): SerialPortInfo[] {
+export function getPorts(): Promise<SerialPortInfo[]> {
     if (Deno.build.os === "windows") {
-        return getPortsWin()
+        return import("./windows/serial_port.ts").then(mod=>mod.getPortsWin())
     } else if (Deno.build.os === "darwin") {
-        return getPortsDarwin()
+        return import("./darwin/serial_port.ts").then(mod=>mod.getPortsDarwin())
     } else if (Deno.build.os === "linux") {
-        return getPortsLinux()
+        return import("./linux/serial_port.ts").then(mod=>mod.getPortsLinux())
     } else {
         throw new Error(`Unsupported OS: ${Deno.build.os}`)
     }
 }
 
-export function open(name: string | SerialPortInfo, options: SerialOptions): Promise<SerialPort> {
+export async function open(name: string | SerialPortInfo, options: SerialOptions): Promise<SerialPort> {
     name = (name?.name ?? name) as string
     if (Deno.build.os === "windows") {
+        const { SerialPortWin } = await import("./windows/serial_port.ts")
         const port = new SerialPortWin(name)
         return port.open(options).then(()=>port)
     } else if (Deno.build.os === "darwin") {
+        const { SerialPortDarwin } = await import("./darwin/serial_port.ts")
         const port = new SerialPortDarwin(name)
         return port.open(options).then(()=>port)
     } else if (Deno.build.os === "linux") {
+        const { SerialPortLinux } = await import("./linux/serial_port.ts")
         const port = new SerialPortLinux(name)
         return port.open(options).then(()=>port)
     } else {
